46e220d57ff180f047bcf5f7eb4ff429f9f62462
Hapbi <seashihoxd0987@Gmail.com>
2026-01-30 09:37:07 +0700

    Update EX1.py

--- chapter1/EX1.py
+++ chapter1/EX1.py
@@ -1,1 +1,9 @@
-nothing, will update tomorrow
+int = 10
+float = 3.14
+str = "Test.io"
+# f str
+print(f"Xin chào tên tôi là {str}, tôi {int} tuổi, có {float} năm kinh nghiệm")
+
+print("xin chào",str)
+print("PI = ",float)
+print("1 đến ",int)
