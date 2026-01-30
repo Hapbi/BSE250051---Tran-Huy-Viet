2274d52c8bf680fdb328968e1801e9b975b9ac70
Hapbi <seashihoxd0987@Gmail.com>
2026-01-30 10:11:16 +0700

    Update Ex5.py

--- chapter1/Ex5.py
+++ chapter1/Ex5.py
@@ -1,1 +1,16 @@
+ten = "Test.io"
+tuoi = 18
+average_score = 8.5
 
+print("biến của tên",type(ten))
+print("biến của tuổi",type(tuoi))
+print("biến của average_score",type(average_score))
+
+age_next_year = tuoi + 1
+doubled_scor = age_next_year * 2
+
+print("Thông tin cá nhân")
+print("Tên là", ten,type(ten))
+print(f"sang năm {ten}, sẽ là {age_next_year} tuổi", type(tuoi))
+print(f"chục năm nữa {ten}, sẽ là {doubled_scor} tuổi", type(tuoi))
+print(f"điểm số trung bình trên lớp {average_score}", type(average_score))    
