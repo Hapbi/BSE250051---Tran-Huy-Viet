32f1650a510105aaa1b13ab203a3ed7c7718d9b4
Hapbi <seashihoxd0987@Gmail.com>
2026-02-01 22:09:58 +0700

    Update Ex15.py

--- chapter1/Ex15.py
+++ chapter1/Ex15.py
@@ -1,1 +1,9 @@
+for i in range(1, 4):
+    name = input(f"Nhập tên sinh viên {i}: ")
+    toan = float(input("Điểm Toán: "))
+    ly = float(input("Điểm Lý: "))
+    hoa = float(input("Điểm Hóa: "))
+    
+    dtb = (toan + ly + hoa) / 3
+    print(f"Sinh viên: {name}, Điểm trung bình: {dtb:.2f}")
 
